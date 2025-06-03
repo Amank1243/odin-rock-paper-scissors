26ff7d7b63c83997a57453334eccb49ba159d616
Aman Khajuria <24khajuriaa@gmail.com>
2025-06-02 19:54:07 -0700

    Added to script.js. Debugging playRound func

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
 function getComputerChoice () {
     // randomly return the following strings “rock”, “paper” or “scissors”.
-    let string
+    let computer;
     let num = Math.random()
     if (num <= 0.333333) {
-        string = "rock"
+        computer = "rock";
     } else if (num > 0.333333 && num <= 0.66666 ) {
-        string = "paper"
+        computer = "paper";
     } else if (num > 0.666666 && num <= 1) {
-        string = "scissors"
+        computer = "scissors";
     }
-    return string
+    return computer;
 }
 
+function getHumanChoice () {
+    let human = prompt("Choose rock, paper, or scissors ")
+    return "Your choice was " + human;
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanAnswer = human;
+let computerAnswer = computer;
+
+// let humanChoice = getHumanChoice();
+// let computerChoice = getComputerChoice();
+
+function PlayRound (humanChoice, computerChoice) {
+    if (humanChoice == "rock" || humanChoice == "Rock" || humanChoice == "ROCK" &&
+        computerChoice == "paper" || computerChoice == "Paper" || computerChoice == "PAPER") {
+            computerScore++;
+            return "You loose! Paper beats rock";
+        }
+    if (humanChoice == "paper" || humanChoice == "Paper" || humanChoice == "PAPER" &&
+        computerChoice == "rock" || computerChoice == "Rock" || computerChoice == "ROCK") {
+            humanScore++;
+            return "You win! Paper beats rock";
+        }
+     if (humanChoice == "scissors" || humanChoice == "Scissors" || humanChoice == "SCISSORS" &&
+        computerChoice == "paper" || computerChoice == "Paper" || computerChoice == "PAPER") {
+            humanScore++;
+            return "You win! Scissors beats paper";
+        }
+    if (humanChoice == "paper" || humanChoice == "Paper" || humanChoice == "PAPER" &&
+        computerChoice == "scissors" || computerChoice == "Scissors" || computerChoice == "SCISSORS") {
+            computerScore++;
+            return "You loose! Scissors beats paper";
+        }
+    if (humanChoice == "rock" || humanChoice == "Rock" || humanChoice == "ROCK" &&
+        computerChoice == "scissors" || computerChoice == "Scissors" || computerChoice == "SCISSORS") {
+            humanScore++;
+            return "You win! Rock beats scissors";
+        }
+    if (humanChoice == "scissors" || humanChoice == "Scissors" || humanChoice == "SCISSORS" &&
+        computerChoice == "rock" || computerChoice == "Rock" || computerChoice == "ROCK") {
+            computerScore++;
+            return "You loose! Rock beats scissors";
+        }
+}
+// Try to play one round. Figure out how to get human function to prompt twice
+console.log(PlayRound(humanAnswer, computerAnswer));
